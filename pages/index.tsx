1e78074ad4169e1a15b2d7f7ad96417690a4a6bb
saezuri510 <saezuru510@gmail.com>
2023-01-19 18:59:44 +0900

    remove: 不要なファイルを削除しました。

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
-
 const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-)
+  <div>
+    <div>Hello world</div>
+  </div>
+);
 
-export default IndexPage
+export default IndexPage;
